878edea664893b47ae88e348fa5cacc164a49a85
FazioFrancesco <55951469+faziofrancesco@users.noreply.github.com>
2020-10-08 19:39:04 +0200

    update 1.0

--- src/components/managereferences/Add.js
+++ src/components/managereferences/Add.js
@@ -18,24 +18,22 @@ export default class Add extends React.Component {
 
             var sample = bibtexParse.toJSON(text);
             sample[0].entryTags["@type"] = sample[0].entryType
-            const data = {entryTags: sample[0].entryTags};
+
 
             fetch('/prova', {
                 method: 'POST', // or 'PUT'
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(data),
+                body: JSON.stringify(sample[0].entryTags),
             })
                 .then(response => response.json())
-                .then(data => {
-                    console.log('Success:', data);
+                .then(dati => {
+                    console.log('Success:', dati);
                 })
                 .catch((error) => {
                     console.error('Error:', error);
                 });
-            console.log(sample[0].entryType);
-            console.log(sample[0].entryTags);
 
 
         };
--- src/components/managereferences/article.js
+++ src/components/managereferences/article.js
@@ -1,62 +1,156 @@
 import React from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
 export default class Article extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            values: {
+                Author: "",
+                Title: "",
+                Journal: "",
+                Year: "",
+                Volume: "",
+                Number: "",
+                Pages: ""
+            },
+            isSubmitting: false,
+            isError: false
 
-        // var bibtexParse = require('bibtex-parse-js');
+        };
 
-        // var sample = bibtexParse.toJSON();
 
-        // console.log(sample);
     }
+
+    submitForm = async e => {
+        e.preventDefault();
+        console.log(this.state);
+        this.setState({isSubmitting: true});
+        let v = this.state.values;
+        v["@type"] = "article";
+        const res = await fetch("/prova", {
+
+            method: "POST",
+            body: JSON.stringify(v),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        this.setState({isSubmitting: false});
+        const data = await res.json();
+        !data.hasOwnProperty("error")
+            ? this.setState({message: data.success})
+            : this.setState({message: data.error, isError: true});
+
+        setTimeout(
+            () =>
+                this.setState({
+                    isError: false,
+                    message: "",
+                    values: {Author: "", Title: "", Journal: "", Year: "", Volume: "", Number: "", Pages: ""}
+                }),
+            1600
+        );
+    };
+
+    handleInputChange = e =>
+        this.setState({
+            values: {...this.state.values, [e.target.name]: e.target.value}
+        });
 
     render() {
         return (
             <div>
-                <Form>
-                    <Form.Group controlId="formAuthor">
-                        <Form.Label>Author</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Author"/>
-                    </Form.Group>
+                <form onSubmit={this.submitForm}>
+                    <div className="input-group">
+                        <label htmlFor="Author">Author</label>
+                        <input
+                            type="text"
+                            name="Author"
+                            id="Author"
+                            value={this.state.values.Author}
+                            onChange={this.handleInputChange}
+                            title="Author"
 
-                    <Form.Group controlId="formTitle">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" placeholder="Enter title"/>
-                    </Form.Group>
-                    <Form.Group controlId="formJournal">
-                        <Form.Label>Journal</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Journal"/>
-                    </Form.Group>
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="Title">Title</label>
+                        <input
+                            type="text"
+                            name="Title"
+                            id="Title"
+                            value={this.state.values.Title}
+                            onChange={this.handleInputChange}
+                            title="Title"
 
-                    <Form.Group controlId="formYear">
-                        <Form.Label>Year</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Year"/>
-                    </Form.Group>
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="Journal">Journal</label>
+                        <input
+                            type="text"
+                            name="Journal"
+                            id="Journal"
+                            value={this.state.values.Journal}
+                            onChange={this.handleInputChange}
+                            title="Journal"
 
-                    <Form.Group controlId="formVolume">
-                        <Form.Label>Volume</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Volume"/>
-                    </Form.Group>
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="Year">Year</label>
+                        <input
+                            type="number"
+                            name="Year"
+                            id="Year"
+                            value={this.state.values.Year}
+                            onChange={this.handleInputChange}
+                            title="Year"
 
-                    <Form.Group controlId="formNumber">
-                        <Form.Label>Number</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Number"/>
-                    </Form.Group>
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="Volume">Volume</label>
+                        <input
+                            type="text"
+                            name="Volume"
+                            id="Volume"
+                            value={this.state.values.Volume}
+                            onChange={this.handleInputChange}
+                            title="Volume"
 
-                    <Form.Group controlId="formPages">
-                        <Form.Label>Pages</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Pages"/>
-                    </Form.Group>
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="Number">Number</label>
+                        <input
+                            type="text"
+                            name="Number"
+                            id="Number"
+                            value={this.state.values.Number}
+                            onChange={this.handleInputChange}
+                            title="Number"
 
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="Pages">Pages</label>
+                        <input
+                            type="text"
+                            name="Pages"
+                            id="Pages"
+                            value={this.state.values.Pages}
+                            onChange={this.handleInputChange}
+                            title="Pages"
+
+                        />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+                <div className={`message ${this.state.isError && "error"}`}>
+                    {this.state.isSubmitting ? "Submitting..." : this.state.message}
+                </div>
             </div>
         );
     }
-
 }
